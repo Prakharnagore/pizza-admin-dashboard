0cea3aadd17dddd1f83edd4e91ec6ce6999790dc
Prakhar Nagore <prakharnagore111@gmail.com>
2024-07-27 20:40:37 +0530

    add login form

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -14,7 +14,7 @@ describe("Login Page", () => {
     expect(screen.getByText("Sign in")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Username")).toBeInTheDocument();
     expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
     expect(
       screen.getByRole("checkbox", { name: "Remember me" })
     ).toBeInTheDocument();
